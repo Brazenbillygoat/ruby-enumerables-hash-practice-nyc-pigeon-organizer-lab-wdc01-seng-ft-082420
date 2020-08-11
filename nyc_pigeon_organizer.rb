aa0f9f01cda8a7bebd5d0048ea6c0399f26b1451
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-11 18:50:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,39 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+
+  new_array = {}
+  pigeon_names_array = data[:gender][:male].concat( data[:gender][:female])
+  
+  pigeon_names_array.each do |name| 
+    new_array["#{name}"] = {color: [], gender: [], lives: [] }
+    
+    data[:color].each do |key, value|
+      key_to_string = "#{key}"
+
+      if value.include? name
+        new_array["#{name}"][:color].push(key_to_string)
+      end
+
+    end
+
+    if data[:gender][:male].include? name
+      new_array["#{name}"][:gender].push("male")
+    else
+      new_array["#{name}"][:gender].push("female")
+    end
+
+    if data[:lives]["Subway"].include? name
+      new_array["#{name}"][:lives].push("Subway")
+    elsif data[:lives]["Central Park"].include? name
+      new_array["#{name}"][:lives].push("Central Park")
+    elsif data[:lives]["Library"].include? name
+      new_array["#{name}"][:lives].push("Library")
+    elsif data[:lives]["City Hall"].include? name
+      new_array["#{name}"][:lives].push("City Hall")
+    end
+  
+  end
+
+  return new_array
 end
+
